ab34f8df7fc334e391b0627c16797790a32d7c6e
Suriyarithik <suriyavedsum@gmail.com>
2017-07-24 14:33:14 +0530

    Create proset66.java

---
+++ proset66.java
@@ -0,0 +1,29 @@
+# proset66
+
+import java.util.Scanner;
+public class proset66
+{
+public static void main(String[] args) {
+   Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int k= sc.nextInt();
+    int i;
+    int sum=0;
+    int arr[]=new int[n];
+    for(i=0;i<n;i++){
+      arr[i]=sc.nextInt();
+      if(i!=k){
+        sum=sum+arr[i];
+      }
+    }
+    int b=sc.nextInt();//charged
+    sum=sum/2;
+    if(sum==b){
+      System.out.println("Bon Appetit");
+    }
+    else{
+      System.out.println(Math.abs(sum-b));
+    }
+
+  }
+}
